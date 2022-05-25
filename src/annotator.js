3a9cb9ce4fc8bd1ccaf83c0b1e517a0d5787450a
Connor Cooper <cooper.connor.17@gmail.com>
2022-05-25 22:42:27 +1000

    Separate out canvas logic

--- src/annotator.js
+++ src/annotator.js
@@ -1,1 +1,55 @@
 import * as utils from './utils.js'
+
+let canvas_defaults = {
+  uniformScaling: false
+}
+
+let defaults = {
+  fill: 'rgba(0,0,0,0)',
+  originX: 'left',
+  originY: 'top',
+  width: 75,
+  height: 75,
+  strokeWidth: 2,
+  strokeUniform: true,
+}
+
+export class Annotator {
+  constructor(canvas, num_frames) {
+    this.canvas = new fabric.Canvas(canvas, canvas_defaults);
+    this.num_frames = num_frames
+    this.current_frame = 0;
+
+    this.tracks = {};
+    this.frames = [];
+    for (let i = 0; i < num_frames; i++) {
+      this.frames.push({})
+    }
+  }
+
+  set_frame(frame_id) {
+    let frame = this.frames[this.current_frame]
+    Object.keys(frame).forEach((id) => {
+      frame[id].set({visible: false})
+    })
+    this.current_frame = frame_id
+    frame = this.frames[this.current_frame]
+    Object.keys(frame).forEach((id) => {
+      frame[id].set({visible: true})
+    })
+  }
+
+  new_box(frame_id, track_id, properties={}) {
+    Object.assign(properties, defaults);
+    let rect = new fabric.Rect(properties);
+
+    if (!(track_id in this.tracks)) {
+      this.tracks[track_id] = {};
+    }
+    this.tracks[track_id][frame_id] = rect
+    this.frames[frame_id][track_id] = rect
+
+    this.canvas.add(rect)
+    return rect
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,80 +1,29 @@
 // TODO disable selection box while drag-adding a box
 // http://jsfiddle.net/a7mad24/aPLq5/
-import * as annotator from "./annotator.js"
+import {Annotator} from "./annotator.js"
 
 let num_frames = 50;
-let current_frame = 0;
 
 let is_down, mouse_x, mouse_y;
 let drag_rect, orig_x, orig_y; // For when drag-creating new boxes
 
 let new_track_id = 0;
 
-let canvas_defaults = {
-  uniformScaling: false
-}
-
-let defaults = {
-  fill: 'rgba(0,0,0,0)',
-  originX: 'left',
-  originY: 'top',
-  width: 75,
-  height: 75,
-  strokeWidth: 2,
-  strokeUniform: true,
-}
-
-let canvas = new fabric.Canvas('canvas', canvas_defaults);
-
-let frames = []
-for (let i = 0; i < num_frames; i++) {
-  frames.push({})
-}
-
-let tracks = {}
-
-function setFrame(frame_id) {
-  console.log(current_frame)
-  let frame = frames[current_frame]
-  Object.keys(frame).forEach((id) => {
-    frame[id].set({visible: false})
-  })
-  current_frame = frame_id
-  frame = frames[current_frame]
-  Object.keys(frame).forEach((id) => {
-    frame[id].set({visible: true})
-  })
-  console.log(current_frame)
-  console.log(frame)
-}
-
-function newBox(frame_id, track_id, properties={}) {
-  Object.assign(properties, defaults);
-  let rect = new fabric.Rect(properties);
-
-  if (!(track_id in tracks)) {
-    tracks[track_id] = {};
-  }
-  tracks[track_id][frame_id] = rect
-  frames[frame_id][track_id] = rect
-
-  canvas.add(rect)
-  return rect
-}
+let annotator = new Annotator('canvas', num_frames);
 
 updateUI();
 
-canvas.on('mouse:down', (o) => {
+annotator.canvas.on('mouse:down', (o) => {
   if (o.target) {
     return;
   }
 
   is_down = true;
-  let pointer = canvas.getPointer(o.e);
+  let pointer = annotator.canvas.getPointer(o.e);
   orig_x = pointer.x;
   orig_y = pointer.y;
   new_track_id += 1;
-  drag_rect = newBox(current_frame, new_track_id, {
+  drag_rect = annotator.new_box(annotator.current_frame, new_track_id, {
     left: orig_x,
     top: orig_y,
     width: pointer.x-orig_x,
@@ -82,8 +31,8 @@ canvas.on('mouse:down', (o) => {
   })
 })
 
-canvas.on('mouse:move', (o) => {
-  let pointer = canvas.getPointer(o.e);
+annotator.canvas.on('mouse:move', (o) => {
+  let pointer = annotator.canvas.getPointer(o.e);
   mouse_x = pointer.x
   mouse_y = pointer.y
 
@@ -96,32 +45,32 @@ canvas.on('mouse:move', (o) => {
                  stroke: 'red'
   });
 
-  canvas.renderAll();
+  annotator.canvas.renderAll();
 })
 
-canvas.on('mouse:up', (o) => {
+annotator.canvas.on('mouse:up', (o) => {
   is_down = false;
 })
 
 function updateUI() {
-  document.getElementById('current_frame').textContent = `Frame index: ${current_frame}/${num_frames-1}`
+  document.getElementById('current_frame').textContent = `Frame index: ${annotator.current_frame}/${annotator.num_frames-1}`
 }
 
 function nextFrame() {
   if (current_frame >= num_frames) return;
-  setFrame(current_frame + 1);
+  annotator.set_frame(annotator.current_frame + 1);
   // Seek ahead in video
   // Load current frame data
-  canvas.renderAll();
+  annotator.canvas.renderAll();
   updateUI();
 }
 
 function prevFrame() {
   if (current_frame <= 0) return;
-  setFrame(current_frame - 1);
+  annotator.set_frame(annotator.current_frame - 1);
   // Seek ahead in video
   // Load current frame data
-  canvas.renderAll();
+  annotator.canvas.renderAll();
   updateUI();
 }
 
